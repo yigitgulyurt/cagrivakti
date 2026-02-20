31172d468dd2a9463c2666d1a66cf1fcf170eb9f
yigitgulyurt <109045170+yigitgulyurt@users.noreply.github.com>
2026-02-20 20:11:47 +0300

    fix speed index

--- app/routes/views.py
+++ app/routes/views.py
@@ -121,7 +121,6 @@ def offline():
     return render_template('utils/offline.html')
 
 @views_bp.route('/sitemap.xml')
-@cache.cached(timeout=86400)
 def serve_sitemap():
     """Dinamik Sitemap oluşturur."""
     from flask import make_response
@@ -174,10 +173,11 @@ def serve_sitemap():
     sitemap_xml = render_template('utils/sitemap_template.xml', pages=pages)
     response = make_response(sitemap_xml)
     response.headers["Content-Type"] = "application/xml"
+    # 24 saat cache
+    response.headers["Cache-Control"] = "public, max-age=86400"
     return response
 
 @views_bp.route('/robots.txt')
-@cache.cached(timeout=86400)
 def serve_robots():
     """robots.txt dosyasını sunar."""
     content = "User-agent: *\n"
@@ -189,6 +189,8 @@ def serve_robots():
     
     response = make_response(content)
     response.headers["Content-Type"] = "text/plain"
+    # 24 saat cache
+    response.headers["Cache-Control"] = "public, max-age=86400"
     return response
 
 @views_bp.route('/sw.js')
