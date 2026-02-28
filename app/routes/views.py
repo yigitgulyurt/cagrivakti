439eb7fcbfd63b185865438beedce825381405ca
yigitgulyurt <109045170+yigitgulyurt@users.noreply.github.com>
2026-03-01 01:28:53 +0300

    Update views.py

--- app/routes/views.py
+++ app/routes/views.py
@@ -138,7 +138,7 @@ def serve_sitemap():
         'views.ramazan_nedir', 'views.orucu_bozan_durumlar', 
         'views.neden_biz', 'views.indir', 'views.konum_bul', 
         'views.iletisim', 'views.ilkelerimiz',
-        'views.bilgi_kosesi_liste'
+        'views.bilgi_kosesi_liste','views.prime_number'
     ]
     
     for rule in static_urls:
