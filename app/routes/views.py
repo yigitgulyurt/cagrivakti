227fd66da3c805a57c7c58f2049307148f1b6035
yigitgulyurt <109045170+yigitgulyurt@users.noreply.github.com>
2026-03-08 04:19:08 +0300

    Update views.py

--- app/routes/views.py
+++ app/routes/views.py
@@ -820,6 +820,7 @@ def generate_id(length=7):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 @views_bp.route('/qr/shorten', methods=['POST'])
+@csrf.exempt
 def shorten():
     return jsonify({'test': 'ok', 'data': request.get_json(silent=True, force=True)})
 
