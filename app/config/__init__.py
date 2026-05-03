c67731ed83499b42f6593464afdb0f8bb4be1461
yigitgulyurt <109045170+yigitgulyurt@users.noreply.github.com>
2026-05-03 20:05:03 +0300

    Update __init__.py

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    APP_VERSION = "2.34"
+    APP_VERSION = "2.35"
     # Tek versiyon kaynağı: APP_VERSION
     SEND_FILE_MAX_AGE_DEFAULT = 31536000 # Flask static dosya cache süresi (1 Yıl)
     SERVER_NAME = os.environ.get('SERVER_NAME')
