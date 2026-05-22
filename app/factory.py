add2560d34ef15a7b6164e812b3da349e2f8a872
yigitgulyurt <109045170+yigitgulyurt@users.noreply.github.com>
2026-05-22 23:27:59 +0300

    7.01
    versiyon yer  değişimi

--- app/factory.py
+++ app/factory.py
@@ -44,7 +44,7 @@ def create_app(config_class=Config):
                 "http://127.0.0.1:*"
             ]
         },
-        r"/cagri_vakitleri": {
+        r"/api/cagri_vakitleri": {
             "origins": [
                 "https://cagrivakti.com.tr",
                 "https://www.cagrivakti.com.tr",
